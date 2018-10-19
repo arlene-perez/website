94900d4749bf96e1495d62e5693feffd4395057c
arlene-perez <arlene.perez@nbcuni.com>
2018-10-19 11:11:00 -0700

    Initial setup for repo

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-home"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>UNIITED BROKERS REALTY</h1>
+                <p>Our professional staff is ready to help you with all your Real Estate needs
+                including Escrow and Mortgage services. We serve all areas, especially
+                L.A. and Orange County.</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
